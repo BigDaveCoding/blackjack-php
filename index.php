9b616fb393b72b7f0551384284d248bda608debc
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-19 20:08:35 +0000

    testing displaying properties of Card object

--- index.php
+++ index.php
@@ -5,5 +5,15 @@ include './Classes/Deck.php';
 
 $deckTest = new Deck();
 
+//$randomIndex = $deckTest->randomCardIndex();
+//$randomCardFromDeck = $deckTest->drawCard($randomIndex);
+$randomCardTest = $deckTest->drawCard();
+
 echo "<pre>";
+echo $randomCardTest->name;
+echo $randomCardTest->suit;
+echo $randomCardTest->score;
+echo "<br />";
+var_dump($randomCardTest);
+echo "<br />";
 var_dump($deckTest);
